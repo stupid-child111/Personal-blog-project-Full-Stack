9fcecd32b49d84db9aafc009b971e99dfc626ccd
stupid-child111 <3468619937@qq.com>
2025-11-14 21:28:47 +0800

    feat:Pager(分页组件实现)以及readme文档更新相关知识

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,16 +3,24 @@
     你好
     <Avatar url="https://www.dmoe.cc/random.php" :size="100" />
     <Icon type="home" />
+    <Pager :total="total" :current="current" @pageChange="handlePageChange" />
   </div>
 </template>
 
 <script setup>
 import Avatar from "./components/Avatar.vue";
 import Icon from "./components/Icon.vue";
+import Pager from "./components/Pager.vue";
+import { ref } from "vue";
+const total = ref(302);
+const current = ref(3);
+const handlePageChange = ((page) => {
+  current.value = page;
+})
 </script>
 
 <style scoped>
 .iconfont {
   color: red;
 }
-  </style>
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+import './styles/style.less'
 import App from './App.vue'
 
 createApp(App).mount('#app')
