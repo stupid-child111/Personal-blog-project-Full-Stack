67c9438ae9c039858cabd306ca35fbe76ad33384
stupid-child111 <3468619937@qq.com>
2025-11-14 16:43:34 +0800

    feat:icon组件开发以及相关内容解释

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,11 +2,17 @@
   <div>
     你好
     <Avatar url="https://www.dmoe.cc/random.php" :size="100" />
+    <Icon type="home" />
   </div>
 </template>
 
 <script setup>
 import Avatar from "./components/Avatar.vue";
+import Icon from "./components/Icon.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.iconfont {
+  color: red;
+}
+  </style>
---
+++ frontend/src/components/Icon.vue
@@ -0,0 +1,44 @@
+<template>
+    <i class="iconfont" :class="iconClass"></i>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const classMap = {
+  home: "iconzhuye",
+  success: "iconzhengque",
+  error: "iconcuowu",
+  close: "iconguanbi",
+  warn: "iconjinggao",
+  info: "iconxinxi",
+  blog: "iconblog",
+  code: "iconcode",
+  about: "iconset_about_hov",
+  weixin: "iconweixin",
+  mail: "iconemail",
+  github: "icongithub",
+  qq: "iconsign_qq",
+  arrowUp: "iconiconfonticonfonti2copy",
+  arrowDown: "iconiconfonticonfonti2",
+  empty: "iconempty",
+  chat: "iconliuyan",
+};
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+  },
+});
+
+const iconClass = computed(() => {
+  return classMap[props.type];
+});
+</script>
+
+<style scoped>
+/* 示例图标 */
+@import "//at.alicdn.com/t/font_2164449_nalfgtq7il.css"; 
+/* 自定义图标集 可根据需要添加 */   
+/* @import "//at.alicdn.com/t/c/font_5065705_ccn3k8xgb2v.css"; */
+</style>
