8f886371268d1d7652d92ea959381e0dc18ef8e3
stupid-child111 <3468619937@qq.com>
2025-11-17 23:55:12 +0800

    feat:elementPlus基本使用

--- admin/README.md
+++ admin/README.md
@@ -1,3 +1,5 @@
 # 个人博客全栈项目(管理后台)
 
 ### 技术栈：Vue3 + ElementPlus
+
+### 引入 ElementPlus 组件库
--- admin/src/main.js
+++ admin/src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+createApp(App).use(ElementPlus).mount("#app");
