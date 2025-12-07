9ee8c85f103662d125210c8d619483a009bb2a4b
stupid-child111 <3468619937@qq.com>
2025-12-08 01:07:05 +0800

    refactor：清除创建新分支时，拉取的main分支的代码

--- frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import "./styles/style.less";
-import App from "./App.vue";
-import router from "./router";
-import './mock';
-
-createApp(App).use(router).mount("#app");
