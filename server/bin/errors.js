87cba512ad35839505f18abe23e084c0674bb599
Mingyue <myxray@starways.team>
2025-12-09 04:10:37 +0800

    feat：自定义错误类&&配置env文件

---
+++ server/bin/errors.js
@@ -0,0 +1,51 @@
+//自定义处理
+
+class ServiceError extends Error {
+  /***
+   * @param {string} message 错误消息
+   * @param {number} code 错误码
+   */
+  constructor(message, code) {
+    super(message);
+    this.code = code;
+  }
+
+  //方法
+  toResponseJSON() {}
+}
+
+//文件上传错误
+
+exports.UploadError = class extends ServiceError {
+  constructor(message, code) {
+    super(message, 413);
+  }
+};
+
+//禁止访问错误
+exports.ForbiddenError = class extends ServiceError {
+  constructor(message, code) {
+    super(message, 403);
+  }
+};
+//验证错误
+exports.ValidationError = class extends ServiceError {
+  constructor(message, code) {
+    super(message, 406);
+  }
+};
+//无资源错误
+exports.NotFoundError = class extends ServiceError {
+  constructor(message, code) {
+    super("Not Found", 404);
+  }
+};
+
+//未知错误
+exports.UnknownError = class extends ServiceError {
+  constructor(message, code) {
+    super(message, 500);
+  }
+};
+
+module.exports.ServiceError = ServiceError;
--- server/dao/dbConnect.js
+++ server/dao/dbConnect.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("server", "root", "111111", {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
   logging: false,
 });
